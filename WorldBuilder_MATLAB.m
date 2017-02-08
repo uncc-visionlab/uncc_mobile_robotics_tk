fa0a33fd40797d1e197751372bb3cf11a318aabb
arwillis <arwillis@uncc.edu>
2017-02-08 00:49:41 -0500

    Added buttons to the interface and cleaned up the code.

--- WorldBuilder_MATLAB.m
+++ WorldBuilder_MATLAB.m
@@ -37,6 +37,9 @@ classdef WorldBuilder_MATLAB < handle
         
         % Kobuki robot class
         kobuki
+        
+        randomLocationButton
+        resetSimulationButton
     end
     
     methods (Static)
@@ -156,59 +159,7 @@ classdef WorldBuilder_MATLAB < handle
                 kobuki.laserScanListener.setCallbackRate(0.4, world_mat.tfmgr);
                 kobuki.rgbCamListener.setCallbackRate(4, world_mat.tfmgr);
                 kobuki.velocityController.setCallbackRate(0.1, world_mat.tfmgr);
-
-                if (1==0)
-                    controller = robotics.PurePursuit('Waypoints', path);
-                    controller.DesiredLinearVelocity = 0.4;
-                    controlRate = robotics.Rate(10);
-                    goalRadius = 0.1;
-                    robotCurrentLocation = path(1,:);
-                    robotGoal = path(end,:);
-                    distanceToGoal = norm(robotCurrentLocation - robotGoal);
-                    updateCounter = 1;
-                    while( distanceToGoal > goalRadius )
-                        % Receive a new laser sensor reading
-                        scan = receive(scanSub);
-                        
-                        % Get robot pose at the time of sensor reading
-                        pose = getTransform(tftree, 'map', 'robot_base', scan.Header.Stamp, 'Timeout', 2);
-                        
-                        % Convert robot pose to 1x3 vector [x y yaw]
-                        position = [pose.Transform.Translation.X, pose.Transform.Translation.Y];
-                        orientation =  quat2eul([pose.Transform.Rotation.W, pose.Transform.Rotation.X, ...
-                            pose.Transform.Rotation.Y, pose.Transform.Rotation.Z], 'ZYX');
-                        robotPose = [position, orientation(1)];
-                        
-                        % Extract the ranges and angles from the laser scan message.
-                        ranges = scan.Ranges;
-                        angles = scan.readScanAngles;
-                        ranges(isnan(ranges)) = sim.LaserSensor.MaxRange;
-                        
-                        % Insert the laser range observation in the map
-                        insertRay(map, robotPose, ranges, angles, sim.LaserSensor.MaxRange);
-                        
-                        % Compute the linear and angular velocity of the robot and publish it
-                        % to drive the robot.
-                        [v, w] = controller(robotPose);
-                        velMsg.Linear.X = v;
-                        velMsg.Angular.Z = w;
-                        send(velPub, velMsg);
-                        
-                        % Visualize the map after every 50th update.
-                        if ~mod(updateCounter,50)
-                            mapHandle.CData = occupancyMatrix(map);
-                            title(axesHandle, ['OccupancyGrid: Update ' num2str(updateCounter)]);
-                        end
-                        
-                        % Update the counter and distance to goal
-                        updateCounter = updateCounter+1;
-                        distanceToGoal = norm(robotPose(1:2) - robotGoal);
-                        
-                        % Wait for control rate to ensure 10 Hz rate
-                        waitfor(controlRate);
-                    end
-                end
-            end
+            end            
         end
     end
     methods
@@ -218,6 +169,21 @@ classdef WorldBuilder_MATLAB < handle
                 0.1 0.1 -0.1 -0.1 0.1];
             obj.numPolygons = 0;
             obj.VERBOSE = 1;
+            % Create buttons in the GUI window
+            obj.randomLocationButton = uicontrol(figure(1), ...
+                'Style', 'pushbutton', ...
+                'String','Randomize Location', ...
+                'Units', 'normalized', ...
+                'Position', [0.80 0.01 0.2 0.05], ...
+                'Callback', @obj.randomLocationButtonCallback);
+            
+            obj.resetSimulationButton = uicontrol(figure(1), ...
+                'Style', 'pushbutton', ...
+                'String','Reset Simulation', ...
+                'Units', 'normalized', ...
+                'Position', [0.0 0.01 0.2 0.05], ...
+                'Callback', @obj.resetSimulationButtonCallback);
+            
         end
         
         function setGazeboBuilder(obj, gazbuilder)
@@ -292,4 +258,18 @@ classdef WorldBuilder_MATLAB < handle
         end
         
     end
+    %% All GUI-related methods
+    methods (Access = private)
+        function randomLocationButtonCallback(obj, ~, ~)
+            %randomLocationButtonCallback Callback when user presses "Randomize location" button
+            %randomizeLocation(obj);
+            disp('Random location');
+        end
+        
+        function resetSimulationButtonCallback(obj, ~, ~)
+            %resetSimulationButtonCallback Callback when user presses "Reset simulation" button
+            %resetSimulation(obj);
+            disp('Reset sim');
+        end
+    end    
 end
